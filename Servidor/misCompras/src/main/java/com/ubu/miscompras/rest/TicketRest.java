324e48921afc16327c291dbf11883969329509fd
Roberto Miranda Perez <rmp0046@gmail.com>
2015-11-05 18:51:58 +0100

    El servidor Recibe un fichero, no una cadena de texto.

--- Servidor/misCompras/src/main/java/com/ubu/miscompras/rest/TicketRest.java
+++ Servidor/misCompras/src/main/java/com/ubu/miscompras/rest/TicketRest.java
@@ -5,22 +5,88 @@
  */
 package com.ubu.miscompras.rest;
 
+import com.sun.jersey.core.header.ContentDisposition;
+import com.sun.jersey.multipart.FormDataBodyPart;
+import com.sun.jersey.multipart.FormDataParam;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  *
  * @author Roberto
+ *
  */
-@Path("/nuevo")
+@Path("/file")
 public class TicketRest {
-    
+
+    private static final String SERVER_UPLOAD_LOCATION_FOLDER = "C:\\ImagenFolder\\";
+
     @GET
     @Path("/test")
     @Produces(MediaType.TEXT_PLAIN)
     public String test() {
         return "prueba";
+    }
+
+    /**
+     *metodo Post que guarda la imagen en el servidor.
+     *
+     * @param body
+     * @param istream
+     * @return
+     */
+    @POST
+    @Path("/upload")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response uploadFile(@FormDataParam("file") FormDataBodyPart body, @FormDataParam("file") InputStream istream) throws IOException {
+
+        FormDataBodyPart filePart = body;
+        ContentDisposition headerOfFilePart = filePart.getContentDisposition();
+        InputStream fileInputStream = istream;
+
+        String filePath = SERVER_UPLOAD_LOCATION_FOLDER + headerOfFilePart.getFileName();
+        
+        //Guarda el fichero en el Servidor.
+        saveFile(fileInputStream, filePath);
+        istream.close();
+        String output = "File saved to server location using FormDataMultiPart : " + filePath;
+        return Response.status(200).entity(output).build();
+
+    }
+
+    /**
+     * Guarda el impuStream en la ruta Especificada.
+     *
+     * @param uploadedInputStream del Fichero
+     * @param serverLocation ruta donde se guarda el fichero
+     */
+    private void saveFile(InputStream uploadedInputStream, String serverLocation) {
+        try {
+            OutputStream outpuStream = new FileOutputStream(new File(
+                    serverLocation));
+            int read = 0;
+            byte[] bytes = new byte[1024];
+            outpuStream = new FileOutputStream(new File(serverLocation));
+
+            while ((read = uploadedInputStream.read(bytes)) != -1) {
+                outpuStream.write(bytes, 0, read);
+            }
+            outpuStream.flush();
+            outpuStream.close();
+            uploadedInputStream.close();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
